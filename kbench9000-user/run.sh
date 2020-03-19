9a830738b353b7d5d668ba3f3db5f98a559c1945
Karthik Bhargavan <karthikeyan.bhargavan@inria.fr>
2020-03-19 18:39:14 +0100

    run script

--- kbench9000-user/run.sh
+++ kbench9000-user/run.sh
@@ -21,15 +21,8 @@ noturbo() {
 	fi
 }
 
-[[ -e kbench9000.ko ]]
-
 trap "nob_cpus 1; noturbo 0;" INT TERM EXIT
 noturbo 1
 nob_cpus 0
 
-echo "[+] Inserting module to run tests"
-stamp="$(date +%s)"
-insmod kbench9000.ko stamp="$stamp"
-
-echo "[+] Gathering results"
-dmesg | sed -n "s/.*kbench9000: $stamp: \\(.*\\)/\\x1b[37m\\x1b[44m\\x1b[1m\\1\\x1b[0m/p"
+./$1
