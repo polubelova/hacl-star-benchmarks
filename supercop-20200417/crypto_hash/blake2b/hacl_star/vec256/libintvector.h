dcf45173664caaccdd26737d2714d319efeb3399
karthikbhargavan <karthikbhargavan@github.com>
2020-05-04 01:52:22 +0200

    added full libintvector file

--- supercop-20200417/crypto_hash/blake2b/hacl_star/vec256/libintvector.h
+++ supercop-20200417/crypto_hash/blake2b/hacl_star/vec256/libintvector.h
@@ -80,14 +80,24 @@ typedef __m128i Lib_IntVector_Intrinsics_vec128;
 #define Lib_IntVector_Intrinsics_vec128_rotate_left32_24(x0) \
   (_mm_shuffle_epi8(x0, _mm_set_epi8(12,15,14,13,8,11,10,9,4,7,6,5,0,3,2,1)))
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec128_rotate_left32(x0,x1)	\
+  (_mm_rol_epi32(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec128_rotate_left32(x0,x1)	\
   (((x1) == 8? Lib_IntVector_Intrinsics_vec128_rotate_left32_8(x0) : \
    ((x1) == 16? Lib_IntVector_Intrinsics_vec128_rotate_left32_16(x0) : \
    ((x1) == 24? Lib_IntVector_Intrinsics_vec128_rotate_left32_24(x0) : \
     _mm_xor_si128(_mm_slli_epi32(x0,x1),_mm_srli_epi32(x0,32-(x1)))))))
+#endif
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec128_rotate_right32(x0,x1)	\
+  (_mm_ror_epi32(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec128_rotate_right32(x0,x1)	\
   (Lib_IntVector_Intrinsics_vec128_rotate_left32(x0,32-(x1)))
+#endif
 
 #define Lib_IntVector_Intrinsics_vec128_shuffle32(x0, x1, x2, x3, x4)	\
   (_mm_shuffle_epi32(x0, _MM_SHUFFLE(x4,x3,x2,x1)))
@@ -260,11 +270,21 @@ typedef __m256i Lib_IntVector_Intrinsics_vec256;
 #define Lib_IntVector_Intrinsics_vec256_rotate_left32_24(x0) \
   (_mm256_shuffle_epi8(x0, _mm256_set_epi8(12,15,14,13,8,11,10,9,4,7,6,5,0,3,2,1,12,15,14,13,8,11,10,9,4,7,6,5,0,3,2,1)))
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec256_rotate_left32(x0,x1)	\
+  (_mm256_rol_epi32(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec256_rotate_left32(x0,x1)	\
   ((x1 == 8? Lib_IntVector_Intrinsics_vec256_rotate_left32_8(x0) : (x1 == 16? Lib_IntVector_Intrinsics_vec256_rotate_left32_16(x0) : _mm256_or_si256(_mm256_slli_epi32(x0,x1),_mm256_srli_epi32(x0,32-(x1))))))
+#endif
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec256_rotate_right32(x0,x1)	\
+  (_mm256_ror_epi32(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec256_rotate_right32(x0,x1)	\
   (Lib_IntVector_Intrinsics_vec256_rotate_left32(x0,32-(x1)))
+#endif
 
 #define Lib_IntVector_Intrinsics_vec256_rotate_right64_8(x0) \
   (_mm256_shuffle_epi8(x0, _mm256_set_epi8(8,15,14,13,12,11,10,9,0,7,6,5,4,3,2,1,8,15,14,13,12,11,10,9,0,7,6,5,4,3,2,1)))
@@ -287,6 +307,10 @@ typedef __m256i Lib_IntVector_Intrinsics_vec256;
 #define Lib_IntVector_Intrinsics_vec256_rotate_right64_56(x0) \
   (_mm256_shuffle_epi8(x0, _mm256_set_epi8(14,13,12,11,10,9,8,15,6,5,4,3,2,1,0,7,14,13,12,11,10,9,8,15,6,5,4,3,2,1,0,7)))
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec256_rotate_right64(x0,x1)	\
+  (_mm256_ror_epi64(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec256_rotate_right64(x0,x1)	\
   ((x1 == 8? Lib_IntVector_Intrinsics_vec256_rotate_right64_8(x0) : \
    (x1 == 16? Lib_IntVector_Intrinsics_vec256_rotate_right64_16(x0) : \
@@ -296,9 +320,15 @@ typedef __m256i Lib_IntVector_Intrinsics_vec256;
    (x1 == 48? Lib_IntVector_Intrinsics_vec256_rotate_right64_48(x0) : \
    (x1 == 56? Lib_IntVector_Intrinsics_vec256_rotate_right64_56(x0) : \
    _mm256_xor_si256(_mm256_srli_epi64((x0),(x1)),_mm256_slli_epi64((x0),(64-(x1))))))))))))
+#endif
 
+#if defined(__AVX512F__)
+#define Lib_IntVector_Intrinsics_vec256_rotate_left64(x0,x1)	\
+  (_mm256_rol_epi64(x0,x1))
+#else
 #define Lib_IntVector_Intrinsics_vec256_rotate_left64(x0,x1)	\
   (Lib_IntVector_Intrinsics_vec256_rotate_right64(x0,64-(x1)))
+#endif
 
 #define Lib_IntVector_Intrinsics_vec256_shuffle64(x0,  x1, x2, x3, x4)	\
   (_mm256_permute4x64_epi64(x0, _MM_SHUFFLE(x4,x3,x2,x1)))
@@ -315,8 +345,22 @@ typedef __m256i Lib_IntVector_Intrinsics_vec256;
 #define Lib_IntVector_Intrinsics_vec256_load_le(x0) \
   (_mm256_loadu_si256((__m256i*)(x0)))
 
+#define Lib_IntVector_Intrinsics_vec256_load32_be(x0)		\
+  (_mm256_shuffle_epi8(_mm256_loadu_si256((__m256i*)(x0)), _mm256_set_epi8(12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3)))
+
+#define Lib_IntVector_Intrinsics_vec256_load64_be(x0)		\
+  (_mm256_shuffle_epi8(_mm256_loadu_si256((__m256i*)(x0)), _mm256_set_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7)))
+
+
 #define Lib_IntVector_Intrinsics_vec256_store_le(x0, x1) \
   (_mm256_storeu_si256((__m256i*)(x0), x1))
+
+#define Lib_IntVector_Intrinsics_vec256_store32_be(x0, x1)	\
+  (_mm256_storeu_si256((__m256i*)(x0), _mm256_shuffle_epi8(x1, _mm256_set_epi8(12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3))))
+
+#define Lib_IntVector_Intrinsics_vec256_store64_be(x0, x1)	\
+  (_mm256_storeu_si256((__m256i*)(x0), _mm256_shuffle_epi8(x1, _mm256_set_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7))))
+
 
 #define Lib_IntVector_Intrinsics_vec256_insert8(x0, x1, x2)	\
   (_mm256_insert_epi8(x0, x1, x2))
@@ -400,6 +444,137 @@ typedef __m256i Lib_IntVector_Intrinsics_vec256;
 
 #define Lib_IntVector_Intrinsics_vec256_interleave_high128(x1, x2) \
   (_mm256_permute2x128_si256(x1, x2, 0x31))
+
+
+typedef __m512i Lib_IntVector_Intrinsics_vec512;
+
+#define Lib_IntVector_Intrinsics_vec512_zero  \
+  (_mm512_setzero_epi32())
+
+#define Lib_IntVector_Intrinsics_vec512_load32s(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15) \
+  (_mm512_set_epi32(x15,x14,x13,x12,x11,x10,x9,x8,x7,x6,x5,x4,x3,x2,x1,x0)) /* hi lo */
+
+#define Lib_IntVector_Intrinsics_vec512_load64s(x0,x1,x2,x3,x4,x5,x6,x7) \
+  (_mm512_set_epi64(x7,x6,x5,x4,x3,x2,x1,x0)) /* hi lo */
+
+#define Lib_IntVector_Intrinsics_vec512_load32(x) \
+  (_mm512_set1_epi32(x))
+
+#define Lib_IntVector_Intrinsics_vec512_load64(x) \
+  (_mm512_set1_epi64(x))
+
+#define Lib_IntVector_Intrinsics_vec512_insert32(x0, x1, x2) \
+  (x2<8? _mm512_inserti32x8(x0,_mm256_insert_epi32(_mm512_extracti32x8_epi32(x0,0),x1,x2),0) : _mm512_inserti32x8(x0,_mm256_insert_epi32(_mm512_extracti32x8_epi32(x0,1),x1,x2-8),1)) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_extract32(x0, x1) \
+  (x1<8? _mm256_extract_epi32(_mm512_extracti32x8_epi32(x0,0),x1) : _mm256_extract_epi32(_mm512_extracti32x8_epi32(x0,1),x1-8)) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_insert64(x0, x1, x2) \
+  (x2<4? _mm512_inserti64x4(x0,_mm256_insert_epi64(_mm512_extracti64x4_epi64(x0,0),x1,x2),0) : _mm512_inserti64x4(x0,_mm256_insert_epi64(_mm512_extracti64x4_epi64(x0,1),x1,x2-4),1)) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_extract64(x0, x1) \
+  (x1<4? _mm256_extract_epi64(_mm512_extracti64x4_epi64(x0,0),x1) : _mm256_extract_epi64(_mm512_extracti64x4_epi64(x0,1),x1-4)) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_add32(x0, x1) \
+  (_mm512_add_epi32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_add64(x0, x1) \
+  (_mm512_add_epi64(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_sub32(x0, x1) \
+  (_mm512_sub_epi32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_sub64(x0, x1) \
+  (_mm512_sub_epi64(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_mul32(x0, x1) \
+  (_mm512_mul_epu32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_mul64(x0, x1) \
+  (_mm512_mul_epu32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_smul32(x0, x1) \
+  (_mm512_mul_epu32(x0, _mm512_set1_epi32(x1)))
+
+#define Lib_IntVector_Intrinsics_vec512_smul64(x0, x1) \
+  (_mm512_mul_epu32(x0, _mm512_set1_epi64(x1)))
+
+#define Lib_IntVector_Intrinsics_vec512_xor(x0, x1) \
+  (_mm512_xor_si512(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_and(x0, x1) \
+  (_mm512_and_si512(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_or(x0, x1) \
+  (_mm512_or_si512(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_lognot(x0) \
+  (_mm512_xor_si512(x0, _mm512_set1_epi32(-1)))
+
+#define Lib_IntVector_Intrinsics_vec512_shift_left32(x0, x1) \
+  (_mm512_slli_epi32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_shift_left64(x0, x1) \
+  (_mm512_slli_epi64(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_shift_right32(x0, x1) \
+  (_mm512_srli_epi32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_shift_right64(x0, x1) \
+  (_mm512_srli_epi64(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_rotate_left32(x0, x1) \
+  (_mm512_rol_epi32(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_rotate_left64(x0, x1) \
+  (_mm512_rol_epi64(x0, x1))
+
+#define Lib_IntVector_Intrinsics_vec512_eq64(x0, x1) \
+  (_mm512_maskz_broadcastq_epi64(_mm512_cmpeq_epi64_mask(x0, x1), _mm_set1_epi8(0xFF))) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_gt64(x0, x1) \
+  (_mm512_maskz_broadcastq_epi64(_mm512_cmpgt_epi64_mask(x0, x1), _mm_set1_epi8(0xFF))) //TODO : FIX!
+
+#define Lib_IntVector_Intrinsics_vec512_load_le(x0) \
+  (_mm512_loadu_si512((__m512i*)(x0)))
+
+#define Lib_IntVector_Intrinsics_vec512_store_le(x0, x1) \
+  (_mm512_storeu_si512((__m512i*)(x0), x1))
+
+#define Lib_IntVector_Intrinsics_vec512_load64_be(x0) \
+  (_mm512_shuffle_epi8(_mm512_loadu_si512((__m512i*)(x0)), \
+		       _mm512_set_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, \
+				       8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7)))
+
+#define Lib_IntVector_Intrinsics_vec512_store64_be(x0, x1) \
+  (_mm512_storeu_si512((__m512i*)(x0), _mm512_shuffle_epi8(x1, \
+		       _mm512_set_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, \
+				       8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7))))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_low32(x1, x2) \
+  (_mm512_unpacklo_epi32(x1, x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_high32(x1, x2) \
+  (_mm512_unpackhi_epi32(x1, x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_low64(x1, x2) \
+  (_mm512_unpacklo_epi64(x1, x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_high64(x1, x2) \
+  (_mm512_unpackhi_epi64(x1, x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_low128(x1, x2)	\
+  (_mm512_permutex2var_epi64(x1, _mm512_set_epi64(13,12,5,4,9,8,1,0), x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_high128(x1, x2) \
+  (_mm512_permutex2var_epi64(x1, _mm512_set_epi64(15,14,7,6,11,10,3,2), x2))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_low256(x1, x2) \
+  (_mm512_shuffle_i32x4(x1, x2, 0x44))
+
+#define Lib_IntVector_Intrinsics_vec512_interleave_high256(x1, x2) \
+  (_mm512_shuffle_i32x4(x1, x2, 0xee))
+
 
 #elif defined(__aarch64__) || defined(_M_ARM64) || defined(__arm__) || defined(_M_ARM)
 #include <arm_neon.h>
@@ -497,10 +672,10 @@ typedef uint32x4_t Lib_IntVector_Intrinsics_vec128;
 */
 
 #define Lib_IntVector_Intrinsics_vec128_load32_be(x0)		\
-  (vrev32q_u8(vld1q_u32((const uint32_t*)(x0))))
+  (vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(vld1q_u32((const uint32_t*)(x0))))))
 
 #define Lib_IntVector_Intrinsics_vec128_load64_be(x0)		\
-  (vreinterpretq_u32_u64(vrev64q_u8(vld1q_u32((const uint32_t*)(x0)))))
+  (vreinterpretq_u32_u8(vrev64q_u8(vreinterpretq_u8_u32(vld1q_u32((const uint32_t*)(x0))))))
 
 /*
 #define Lib_IntVector_Intrinsics_vec128_store_be(x0, x1)	\
@@ -508,10 +683,10 @@ typedef uint32x4_t Lib_IntVector_Intrinsics_vec128;
 */
 
 #define Lib_IntVector_Intrinsics_vec128_store32_be(x0, x1)	\
-  (vst1q_u32((uint32_t*)(x0),(vrev32q_u8(x1))))
+  (vst1q_u32((uint32_t*)(x0),(vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(x1))))))
 
 #define Lib_IntVector_Intrinsics_vec128_store64_be(x0, x1)	\
-  (vst1q_u32((uint32_t*)(x0),(vrev64q_u8(x1))))
+  (vst1q_u32((uint32_t*)(x0),(vreinterpretq_u32_u8(vrev64q_u8(vreinterpretq_u8_u32(x1))))))
 
 #define Lib_IntVector_Intrinsics_vec128_insert8(x0, x1, x2)	\
   (vsetq_lane_u8(x1,x0,x2))
