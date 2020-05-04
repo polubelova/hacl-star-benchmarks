9b1ff500dc9c425d047181ca1768c0207e60dfc9
karthikbhargavan <karthikbhargavan@github.com>
2020-05-04 11:35:43 +0200

    arm-data

---
+++ arm-data/print.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for file in $( ls data* )
+do
+ egrep -o " ok [0-9]+ [0-9]+ [0-9]+ [^ ]+ [^_]+" "$file" |
+ awk '{printf("%0.2f, %s  %s\n", ($2/16384), $5, $6)}' |
+ sort -t, -k1 -k2  > "${file%}.csv"
+done
