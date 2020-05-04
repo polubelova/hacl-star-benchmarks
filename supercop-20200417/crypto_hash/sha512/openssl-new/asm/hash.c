bc09e7ebd7a949e8b5ccc0a4237dc1584d45bb3c
Marina Polubelova <polubelovam@gmail.com>
2020-05-04 17:17:38 +0200

    add sha512-openssl

---
+++ supercop-20200417/crypto_hash/sha512/openssl-new/asm/hash.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include "crypto_hash.h"
+
+#include <sha.h>
+
+int crypto_hash(unsigned char *out,const unsigned char *in,unsigned long long inlen)
+{
+  SHA512_CTX ctx;
+  SHA512_Init(&ctx);
+  SHA512_Update(&ctx,in,inlen);
+  SHA512_Final(out,&ctx);
+  return 0;
+}
