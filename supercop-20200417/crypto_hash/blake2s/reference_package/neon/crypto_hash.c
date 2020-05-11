f2c873d4336f1bd3833e1130e15ef114a5cb1f81
karthik.bhargavan <karthik.bhargavan@gmail.com>
2020-05-11 20:50:17 +0200

    added blake2 reference package

---
+++ supercop-20200417/crypto_hash/blake2s/reference_package/neon/crypto_hash.c
@@ -0,0 +1,9 @@
+#include "crypto_hash.h"
+#include "crypto_uint32.h"
+#include <stddef.h>
+#include <string.h>
+
+
+int crypto_hash(unsigned char *out, const unsigned char *in, unsigned long long inlen) {
+  return blake2s( out, 32, in, inlen, NULL, 0 );
+}
