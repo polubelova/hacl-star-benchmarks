e30b68ff6314a51d7032a29790c66155f26338e7
Marina Polubelova <polubelovam@gmail.com>
2020-07-31 13:51:48 +0200

    perf numbers without unrolling

---
+++ 300720/xps13-without_unrolling/print-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for file in $( ls data*.raw )
+do
+ egrep -o " ok [0-9]+ [0-9]+ [0-9]+ [^ ]+ [^ ]+" "$file" |
+     sort -n -k2 | awk '{gsub("_-march=native_-mtune=native_-Wno-discarded-qualifiers_-fomit-frame-pointer_-fwrapv_-fPIC_-fPIE", ""); printf("%s %s %0.2f\n", $5, $6, ($2/16384))}' > "${file%}.txt"
+done
